3c01eae5c451b412829b6db69e5412c3e6f99946
Ammon Heinzen <arheinzen@gmail.com>
2016-02-17 15:31:14 -0700

    message-board progress

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
-from .views import MessageListView
+from .views import MessageListView, CommentCreateView, PostView
 
 urlpatterns = [
     
     url(r'^message/$', MessageListView.as_view()),
+    url(r'^post/$', PostView.as_view(success_url="/main/message/")),
+    url(r'^comment/$', CommentCreateView.as_view(success_url="/main/message/")),
     
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View, ListView, DetailView, TemplateView
+from django.views.generic.edit import CreateView
 
 from .models import Message, Comment
 
@@ -10,3 +11,17 @@ class MessageListView(ListView):
     model = Message
     template_name = "message.html"
     context_object_name = "message_list"
+    
+
+class CommentCreateView(CreateView):
+    model = Comment
+    template_name = "comment_create_form.html"
+    fields = ['comments', 'user', 'messages']
+    context_object_name = "comment_list"
+    
+
+class PostView(CreateView):
+    model = Message
+    template_name = "post_create_form.html"
+    fields = '__all__'
+    context_object_name = "message"
