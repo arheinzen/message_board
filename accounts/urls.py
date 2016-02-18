beea86414ed2b14c75ab40d3019129c4156002f7
Ammon Heinzen <arheinzen@gmail.com>
2016-02-18 10:10:40 -0700

    create new user feature added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 
+from .views import SignUp
+
 urlpatterns = [
     
     url(r'^login/$','django.contrib.auth.views.login', name='login', kwargs={'template_name': 'login.html'}),
     url(r'^logout/$','django.contrib.auth.views.logout', name='logout', kwargs={'next_page': '/'}),
+    url(r'^signup/', SignUp.as_view(success_url='/accounts/login/')),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+
+from .forms import CreateUserForm
 
 # Create your views here.
+
+class SignUp(CreateView):
+    form_class = CreateUserForm
+    template_name = 'sign_up.html'
