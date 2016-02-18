fa022309b9e620c21a11f53dea00a3295b12387c
Ammon Heinzen <arheinzen@gmail.com>
2016-02-18 11:07:41 -0700

    pre template commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from .views import SignUp
 urlpatterns = [
     
     url(r'^login/$','django.contrib.auth.views.login', name='login', kwargs={'template_name': 'login.html'}),
-    url(r'^logout/$','django.contrib.auth.views.logout', name='logout', kwargs={'next_page': '/'}),
+    url(r'^logout/$','django.contrib.auth.views.logout', name='logout', kwargs={'next_page': '/main/message'}),
     url(r'^signup/', SignUp.as_view(success_url='/accounts/login/')),
 
 ]
